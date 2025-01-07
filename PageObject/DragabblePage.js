b56ed2f2ad9a2eecc5beb267afb2f2f892cf4976
Jozefmvm <murvitalymvm@gmail.com>
2025-01-07 14:27:55 +0300

    without cursor task

--- PageObject/DragabblePage.js
+++ PageObject/DragabblePage.js
@@ -10,7 +10,32 @@ class DragabblePage extends BasePage{
      this.dragBox = '#dragBox';
      this.borderOfContainer = '[style*="border: 1"]';
      this.h1Text = '[class="text-center"]';
+     this.dragAxisTab ='#draggableExample-tab-axisRestriction';
+     this.onlyXBox = '#restrictedX';
+     this.onlyYBox = '#restrictedY';
+     this.dragContainerTab = '#draggableExample-tab-containerRestriction';
+     this.containedBox = '[class="draggable ui-widget-content ui-draggable ui-draggable-handle"]';
+     this.containedBoxText = '[class="ui-widget-header ui-draggable ui-draggable-handle"]';
+     this.bigContainer = '#containmentWrapper';
+     this.containerText = '[class="draggable ui-widget-content m-3"]';
+     this.dragCursorTab = '#draggableExample-tab-cursorStyle';
+     this.centerBox = '#cursorCenter';
+     this.topLeftBox = '#cursorTopLeft';
+     this.buttomBox = '#cursorBottom';
 
+    }
+
+    async dragDrop(page, originSelector, destinationSelector) {
+
+    const originElement = await page.waitForSelector(originSelector);
+    const destinationElement = await page.waitForSelector(destinationSelector);
+    await originElement.hover();
+    await page.mouse.down();
+    const box = (await destinationElement.boundingBox());
+    await page.mouse.move(box.x + box.width / 2, box.y + box.height / 2);
+    await destinationElement.hover();
+    await page.mouse.up();
+    
     }
 
 
--- tests/dragabblePageTesting.spec.js
+++ tests/dragabblePageTesting.spec.js
@@ -21,5 +21,72 @@ test.beforeEach(async ({ page }, testInfo) => {
     });
 
 
+    test('Should be appropriate position of Xbox after moving along X axis(Axis)', async ({ page }) => {
+  
+      const axisTab = page.locator(dragabblePage.dragAxisTab);
+      await axisTab.click();
+      await dragabblePage.dragDrop(page, dragabblePage.onlyXBox, dragabblePage.onlyYBox);
+      const onlyXBox = page.locator(dragabblePage.onlyXBox);
+      await expect(onlyXBox).toHaveAttribute('style','position: relative; left: 345px; top: 0px;');
+  
+    });
+
+
+    test('Should be appropriate position of Ybox after moving along y axis(Axis)', async ({ page }) => {
+  
+      const axisTab = page.locator(dragabblePage.dragAxisTab);
+      await axisTab.click();
+      await dragabblePage.dragDrop(page, dragabblePage.onlyYBox, dragabblePage.h1Text);
+      const onlyYBox = page.locator(dragabblePage.onlyYBox);
+      await expect(onlyYBox).toHaveAttribute('style','position: relative; left: 0px; top: -164px;');
+  
+    });
+
+
+    test('Should be appropriate position of Xbox after moving along Y axis(Axis)', async ({ page }) => {
+  
+      const axisTab = page.locator(dragabblePage.dragAxisTab);
+      await axisTab.click();
+      await dragabblePage.dragDrop(page, dragabblePage.onlyXBox, dragabblePage.h1Text);
+      const onlyXBox = page.locator(dragabblePage.onlyXBox);
+      await expect(onlyXBox).not.toHaveAttribute('style','position: relative; left: 345px; top: 0px;');
+  
+    });
+
+
+    test('Should be appropriate position of Ybox after moving along x axis(Axis)', async ({ page }) => {
+  
+      const axisTab = page.locator(dragabblePage.dragAxisTab);
+      await axisTab.click();
+      await dragabblePage.dragDrop(page, dragabblePage.onlyYBox, dragabblePage.onlyXBox);
+      const onlyYBox = page.locator(dragabblePage.onlyYBox);
+      await expect(onlyYBox).not.toHaveAttribute('style','position: relative; left: 0px; top: -164px;');
+  
+    });
+
+
+    test('Should be appropriate position of contained box after moving into center of big container axis(Container)', async ({ page }) => {
+  
+      const dragcontainerTab = page.locator(dragabblePage.dragContainerTab);
+      await dragcontainerTab.click();
+      await page.locator(dragabblePage.containedBox).dragTo(page.locator(dragabblePage.bigContainer))
+      const containedBox = page.locator(dragabblePage.containedBox);
+      await expect(containedBox).toHaveAttribute('style','position: relative; left: 343px; top: 59px;');
+  
+    });
+
+
+    test('Should be appropriate position of contained text after moving into center of text container axis(Container)', async ({ page }) => {
+  
+      const dragcontainerTab = page.locator(dragabblePage.dragContainerTab);
+      await dragcontainerTab.click();
+      await page.locator(dragabblePage.containedBoxText).dragTo(page.locator(dragabblePage.containerText))
+      const containedBoxText = page.locator(dragabblePage.containedBoxText);
+      await expect(containedBoxText).toHaveAttribute('style','position: relative; left: 14.2px; top: 55px;');
+  
+    });
+
+
+
 
   });
